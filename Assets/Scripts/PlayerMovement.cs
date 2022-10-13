e4d67e0c609da157ea6cff20fb196c8f85806963
dariaveremey <daria.veremey.v@gmail.com>
2022-10-13 13:22:56 +0300

    Add Jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("Base Settings")]
     [SerializeField] private float _speed = 3f;
     [SerializeField] private CharacterController _controller;
+    [SerializeField] private float _gravityMultiplier = 1f;
 
     [Header("Grounded")]
     [SerializeField] private Transform _checkGroundTransform;
@@ -18,9 +19,24 @@ public class PlayerMovement : MonoBehaviour
 
        Vector3 moveVector = transform.right * horizontal + transform.forward * vertical;
        moveVector *= (_speed * Time.deltaTime);
+       
        _controller.Move(moveVector);
 
        bool isGrounded = Physics.CheckSphere(_checkGroundTransform.position, _checkGroundRadius, _checkGroundMask);
        Debug.LogError($"IsGrounded {isGrounded}");
+       Vector3 fallVector =Vector3.zero;
+       
+       if (isGrounded && Input.GetButtonDown("Jump"))
+       {
+           
+       }
+       
+
+       float gravity = Physics.gravity.y*_gravityMultiplier;
+       fallVector.y += gravity* Time.deltaTime;
+
+       _controller.Move(fallVector);
+       
+
    }
 }
