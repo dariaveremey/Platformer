e57c62e9adbbb697bef02af5f403cd180b7f4ce0
dariaveremey <daria.veremey.v@gmail.com>
2022-10-13 03:08:37 +0300

    Add Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Base Settings")]
     [SerializeField] private float _speed = 3f;
+    [SerializeField] private CharacterController _controller;
+
+    [Header("Grounded")]
+    [SerializeField] private Transform _checkGroundTransform;
+    [SerializeField] private float _checkGroundRadius;
+    [SerializeField] private LayerMask _checkGroundMask;
+    
     private void Update()
    {
        float horizontal = Input.GetAxis("Horizontal");
        float vertical = Input.GetAxis("Vertical");
 
        Vector3 moveVector = transform.right * horizontal + transform.forward * vertical;
-       Vector3 position = transform.position;
-       position += moveVector * (_speed* Time.deltaTime);
-       transform.position = position;
-       
+       moveVector *= (_speed * Time.deltaTime);
+       _controller.Move(moveVector);
+
+       bool isGrounded = Physics.CheckSphere(_checkGroundTransform.position, _checkGroundRadius, _checkGroundMask);
+       Debug.LogError($"IsGrounded {isGrounded}");
    }
 }
